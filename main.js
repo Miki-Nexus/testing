19efe61d5aa5af155c0e15d3c9eb2004484ade87
Mikolaj Jaworski <mjaworski@nexusgeographics.com>
2025-03-06 12:10:08 +0100

    canvis

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 window.addEventListener('load', onLoad);
 
 function onLoad() {
-    console.log('loaded!')
+    console.debug('loaded!');
+
+    const url = new URL(window.location.href);
+    const codi = url.searchParams.get("codi");
+
+    if (!codi) {
+        console.error("no s'ha especificat el codi!");
+        return;
+    }
+
+    console.debug(codi)
+
+    const $p = document.getElementById('codi');
+
+    if ($p) $p.innerText = codi;
 }
