e7aa36007fe3d7b1d00453073671ab06cc21b34d
Mikolaj Jaworski <mjaworski@nexusgeographics.com>
2025-03-07 10:42:32 +0100

    canvis

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-window.addEventListener('load', onLoad);
+const ENDPOINTS = {
+    ce: (codi) => {
+        return `https://gissrv.diba.cat/arcgis/rest/services/SITAC/PAES/MapServer/3/query?where=ce_codi_ine%3D${codi}+and+ce_any%3D2006&spatialRel=esriSpatialRelIntersects&outFields=ce_tendencia&returnGeometry=false&f=pjson`
+    }
+}
 
-function onLoad() {
+onLoad();
+
+async function onLoad() {
     console.debug('loaded!');
 
     const url = new URL(window.location.href);
@@ -13,7 +19,23 @@ function onLoad() {
 
     console.debug(codi)
 
-    const $p = document.getElementById('codi');
+    const res = await fetch(ENDPOINTS.ce(codi)).then(res => res.json()).catch(err => {
+        console.error(err);
 
-    if ($p) $p.innerText = codi;
+        return null;
+    });
+
+    console.debug(res)
+
+    if (!res) return;
+
+    const ce = res.features[0].attributes.CE_TENDENCIA;
+
+    const $p = document.getElementById('ce_valor');
+
+    if ($p) $p.innerText = ce;
+
+    if (ce < -36) $p.classList.add('green');
+    else if (ce > -27 && ce < -36) $p.classList.add('orange');
+    else if (ce > -27) $p.classList.add('red');
 }
