8063a4a90cb990ddc25ed27e33ce7c7d7f3fe9bb
Mikolaj Jaworski <mjaworski@nexusgeographics.com>
2025-03-14 09:38:34 +0100

    canvis

--- constants.js
+++ constants.js
@@ -1,66 +1,60 @@
+export const BASE_URL = 'https://gissrv.diba.cat';
+
 export const ENDPOINTS = {
     exb: objectid => {
         return `https://experience.arcgis.com/experience/492a0462e3154325953ef2dc15b823c6?draft=true#data_s=id%3AdataSource_1-2%3A${objectid}`;
     },
     municipi: codi => {
-        return `https://gissrv.diba.cat/arcgis/rest/services/SITAC/PAES/MapServer/2/query?where=ine%3D${codi}&outFields=objectid&returnGeometry=false&f=pjson`;
+        return `${BASE_URL}/arcgis/rest/services/SITAC/PAES/MapServer/2/query?where=ine%3D${codi}&outFields=objectid,nommuni&returnGeometry=false&f=pjson`;
     },
     /**
      * @param {number} codi del municipi
      * @returns la url per fer la petició
      */
     ce: codi => {
-        return `https://gissrv.diba.cat/arcgis/rest/services/SITAC/PAES/MapServer/3/query?where=ce_codi_ine%3D${codi}+and+ce_any%3D2006&outFields=ce_tendencia&returnGeometry=false&f=pjson`;
+        return `${BASE_URL}/arcgis/rest/services/SITAC/PAES/MapServer/3/query?where=ce_codi_ine%3D${codi}+and+ce_any%3D2006&outFields=ce_tendencia&returnGeometry=false&f=pjson`;
     },
     /**
      * @param {number} codi del municipi
      * @returns la url per fer la petició
      */
     geh: codi => {
-        return `https://gissrv.diba.cat/arcgis/rest/services/SITAC/PAES/MapServer/7/query?where=co2_codi_ine%3D${codi}+and+co2_any%3D2006&outFields=co2_tendencia&returnGeometry=false&f=pjson`;
+        return `${BASE_URL}/arcgis/rest/services/SITAC/PAES/MapServer/7/query?where=co2_codi_ine%3D${codi}+and+co2_any%3D2006&outFields=co2_tendencia&returnGeometry=false&f=pjson`;
     },
     /**
      * @param {number} codi del municipi
      * @returns la url per fer la petició
      */
     per: codi => {
-        return `https://gissrv.diba.cat/arcgis/rest/services/SITAC/PAES/MapServer/11/query?where=eerr_codi_ine%3D${codi}+and+eerr_any%3D2006&outFields=eerr_tendencia&returnGeometry=false&f=pjson`;
+        return `${BASE_URL}/arcgis/rest/services/SITAC/PAES/MapServer/11/query?where=eerr_codi_ine%3D${codi}+and+eerr_any%3D2006&outFields=eerr_tendencia&returnGeometry=false&f=pjson`;
     }
 }
 
 export const VALUES = {
     ce_valor: {
-        green: -36,
-        orange: {
-            min: -27,
-            max: -36
-        },
-        red: -27,
+        min: -36,
+        max: -27,
         /**
          * @param {number} value el valor actual
          * @param {HTMLParagraphElement} $p l'element al qual volem aplicar l'estil corresponent
          */
         applyStyle: function (value, $p) {
-            if (value < this.green) $p.classList.add('green');
-            else if (value > this.orange.min && value < this.orange.max) $p.classList.add('orange');
-            else if (value > this.red) $p.classList.add('red');
+            if (value < this.min) $p.classList.add('green');
+            else if (value > this.max && value < this.min) $p.classList.add('orange');
+            else if (value > this.max) $p.classList.add('red');
         }
     },
     geh_valor: {
-        green: -55,
-        orange: {
-            min: -41.25,
-            max: -55
-        },
-        red: -41.25,
+        min: -55,
+        max: -41.25,
         /**
          * @param {number} value el valor actual
          * @param {HTMLParagraphElement} $p l'element al qual volem aplicar l'estil corresponent
          */
         applyStyle: function (value, $p) {
-            if (value < this.green) $p.classList.add('green');
-            else if (value > this.orange.min && value < this.orange.max) $p.classList.add('orange');
-            else if (value > this.red) $p.classList.add('red');
+            if (value < this.min) $p.classList.add('green');
+            else if (value > this.max && value < this.min) $p.classList.add('orange');
+            else if (value > this.max) $p.classList.add('red');
         }
     },
     per_valor: {
--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import { ENDPOINTS, VALUES } from "./constants.js";
 
+const { animate } = Motion;
+
 onLoad();
 
 async function onLoad() {
@@ -33,19 +35,23 @@ async function onLoad() {
             return null;
         })
     ]).then(res => {
-        if (res[0]) {
-            const oid = res[0].features[0].attributes.OBJECTID;
+        if (res[0] && res[0].features.length > 0) {
+            const attr = res[0].features[0].attributes;
+
+            const $b = document.getElementById('nom-muni');
+
+            if ($b) $b.innerText = attr.NOMMUNI;
 
             /**
              * @type {HTMLAnchorElement}
              */
             const $a = document.getElementById('to-builder');
 
-            $a.href = ENDPOINTS.exb(oid);
+            if ($a) $a.href = ENDPOINTS.exb(attr.OBJECTID);
         }
-        if (res[1]) applyStyleAndValue(res[1], 'ce_valor', 'CE_TENDENCIA');
-        if (res[2]) applyStyleAndValue(res[2], 'geh_valor', 'CO2_TENDENCIA');
-        if (res[3]) applyStyleAndValue(res[3], 'per_valor', 'EERR_TENDENCIA');
+        if (res[1]) applyStyle(res[1], 'ce_valor', 'CE_TENDENCIA');
+        if (res[2]) applyStyle(res[2], 'geh_valor', 'CO2_TENDENCIA');
+        if (res[3]) applyStyle(res[3], 'per_valor', 'EERR_TENDENCIA');
 
         const $loader = document.getElementById('loading');
 
@@ -54,7 +60,11 @@ async function onLoad() {
             
             setTimeout(() => {
                 $loader.remove();
-            }, 505);
+
+                animateValue(res[1], 'ce_valor', 'CE_TENDENCIA');
+                animateValue(res[2], 'geh_valor', 'CO2_TENDENCIA');
+                animateValue(res[3], 'per_valor', 'EERR_TENDENCIA');
+            }, 500);
         }
     }).catch(err => {
         console.error(err);
@@ -67,7 +77,12 @@ async function onLoad() {
  * @param {string} id del element HTML que volem agafar
  * @param {string} field el nom del camp del que volem agafar les dades
  */
-function applyStyleAndValue(response, id, field) {
+function applyStyle(response, id, field) {
+    if (response.features.length === 0) {
+        console.error('No hi ha cap dada en la resposta!');
+        return;
+    }
+
     const value = response.features[0].attributes[field];
 
     const $p = document.getElementById(id);
@@ -77,7 +92,33 @@ function applyStyleAndValue(response, id, field) {
         return;
     }
 
-    $p.innerText = value;
-
     VALUES[id].applyStyle(value, $p);
+}
+
+function animateValue(response, id, field) {
+    if (response.features.length === 0) {
+        console.error('No hi ha cap dada en la resposta!');
+        return;
+    }
+
+    const value = response.features[0].attributes[field];
+
+    const $p = document.getElementById(id);
+
+    if (!$p) {
+        console.error(`No s'ha trobat el paragraf amb id '${id}'!`);
+        return;
+    }
+    
+    animate(0, value, {
+        onUpdate: latest => $p.innerText = latest.toFixed(4),
+        duration: 2.5 + clamp(Math.random(), 0, .5)
+    });
+}
+
+function clamp(value, min, max) {
+    if (value > max) return max;
+    else if (value < min) return min;
+
+    return value;
 }
